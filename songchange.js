87a6c398af967b96cea21f277fb1f9e3ddad68f7
Eda Zhou <16zhoue@gmail.com>
2018-04-18 16:51:18 -0400

    Added pause and play functions when opening and closing tabs

--- songchange.js
+++ songchange.js
@@ -11,5 +11,5 @@ function refreshIframe(src) {
   console.log('executing refresh');
   widget1.next();
   widget1.seekTo(20000);
-  $("#sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/500842647&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true");
+  //$("#sc").attr("src", "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/500842647&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true");
 }
