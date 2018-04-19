5cea65d85acfc5fd90400475e4ac97d784f943e7
Panhavuth Lau <panlau98@gmail.com>
2018-04-19 03:54:58 -0400

    Merge branch 'panhaedits'
    Conflicts:
    	index.html
    	style.css

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ detector.addEventListener("onInitializeSuccess", function() {
 });
 
 function log(node_name, msg) {
-  $(node_name).append("<span>" + msg + "</span><br />")
+  $(node_name).append("<span>" + msg + "</span><br/>")
 }
 
 //function executes when Start button is pushed.
@@ -82,13 +82,13 @@ detector.addEventListener("onImageResultsSuccess", function(faces, image,
       joy = faces[0].emotions.joy;
       console.log(joyduration);
       console.log(mehduration);
-    log('#results', "Expressions: " + JSON.stringify(faces[0].expressions,
-      function(key, val) {
-        return val.toFixed ? Number(val.toFixed(0)) : val;
-      }));
+    // log('#results', "Expressions: " + JSON.stringify(faces[0].expressions,
+    //   function(key, val) {
+    //     return val.toFixed ? Number(val.toFixed(0)) : val;
+    //   }));
 
     // Return an emoji of face
-    log('#results', "Emoji: " + faces[0].emojis.dominantEmoji);
+    // log('#results', "Emoji: " + faces[0].emojis.dominantEmoji);
   }
 });
 
@@ -110,7 +110,7 @@ function drawFeaturePoints(img, featurePoints) {
   }
 }
 
-var iFrequency = 5000; // expressed in miliseconds
+var iFrequency = 100; // expressed in miliseconds
 var myInterval = 0;
 var joyduration = 0, mehduration = 0;
 var counter = 1;
@@ -132,19 +132,19 @@ function getRandomInt(min, max) {
 var i;
 
 function changePlaylist() {
-  if (joy < 40) {
+  if (joy < 30) {
     mehduration++;
     if (joyduration > 0) joyduration = 0;
-    $('#joy-bar').attr('aria-valuenow', joyduration*33).css('width', (joyduration*33)+'%');
-    $('#meh-bar').attr('aria-valuenow', mehduration*33).css('width', (mehduration*33)+'%');
+    $('#joy-bar').attr('aria-valuenow', joyduration*2).css('width', (joyduration*2)+'%');
+    $('#meh-bar').attr('aria-valuenow', mehduration).css('width', (mehduration)+'%');
   }
-  if (joy > 40) {
+  if (joy > 30) {
     joyduration++;
     if (mehduration > 0) mehduration = 0;
-    $('#joy-bar').attr('aria-valuenow', joyduration*33).css('width', (joyduration*33)+'%');
-    $('#meh-bar').attr('aria-valuenow', mehduration*33).css('width', (mehduration*33)+'%');
+    $('#joy-bar').attr('aria-valuenow', joyduration*2).css('width', (joyduration*2)+'%');
+    $('#meh-bar').attr('aria-valuenow', mehduration).css('width', (mehduration)+'%');
   }
-  if (mehduration >= 3) {
+  if (mehduration >= 110) {
     console.log('meh');
     $("#sc").attr("src", playlists[counter%9]);
     counter++;
@@ -153,7 +153,7 @@ function changePlaylist() {
     mehduration = 0;
     $('#meh-bar').attr('aria-valuenow', 100).css('width', 100+'%');
   }
-  if (joyduration >= 3) {
+  if (joyduration >= 55) {
     console.log('user is happy');
     addToPlaylist();
     joyduration = 0;
